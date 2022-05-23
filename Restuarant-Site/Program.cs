fe8b451e7ad1bb5fb7f99cebb8599c3b5c8c6ee7
Ryan Rasi <Ryan.Rasi@avanade.com>
2022-05-23 14:42:00 +0100

    Edited program.cs to make use of controllers and interfaces

--- Restuarant-Site/Program.cs
+++ Restuarant-Site/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Restuarant_Site.Data.Repositories;
+using Restuarant_Site.Models;
+using Restuarant_Site.Services;
+using Restuarant_Site.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<BookingContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICrudRepository<Booking, int>, BookingRepository>();
+builder.Services.AddScoped<ICrudService<Booking, int>, BookingService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RestuarantRestAPI",
+        Version =
+    "v1"
+    });
+});
 
 var app = builder.Build();
 
