d7a4bfbedd5c04830812888f25e585405ac3795c
Ryan Rasi <Ryan.Rasi@avanade.com>
2022-05-23 14:14:22 +0100

    Added BookingContext.cs

---
+++ Restuarant-Site/Data/BookingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restuarant_Site.Models;
+
+namespace Restuarant_Site.Data
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options) : base(options)
+        {
+        }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
